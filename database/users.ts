5fca044f4d5a793a2c06798c85347a2e2d509627
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-06-30 09:35:09 +0200

    Remove console.logs

--- database/users.ts
+++ database/users.ts
@@ -2,7 +2,6 @@ export async function isUserAdminBySessionToken(
   sessionToken: string | undefined,
 ) {
   // FIXME: Implement proper authorization
-  console.log(sessionToken);
   if (sessionToken === 'freddy') return await true;
   return await false;
 }
